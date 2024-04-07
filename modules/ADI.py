6446ee70e54897ca6a5df37c753e8b567ed4e8be
Ken <kenrui.fong@gmail.com>
2024-04-06 21:56:57 -0700

    BC fix 1

--- modules/ADI.py
+++ modules/ADI.py
@@ -29,29 +29,34 @@ def generate_left_matrix_x(C, diffusion, y, BC): #TODO: Memoization tradeoff? Wi
     dt = C.dt/2
     n_grid = C.n_grid
     # Define coefficient clusters
+    def r1(x, y):
+        return (diffusion(x, y) * dt)/(2 * dx**2)
+
+    def r2(x, y):
+        return (diffusion.partial_x(x, y) * dt)/(4 * dx)
+
     def D1(x, y):
-        return - (diffusion(x, y) * dt)/(2 * dx**2) + (diffusion.partial_x(x, y) * dt)/(4 * dx)
-    
+        return - r1(x, y) + r2(x, y)
+
     def D2(x, y):
-        return (diffusion(x, y)* dt)/(dx**2) + 1
+        return 2*r1(x, y) + 1
 
     def D3(x, y):
-        return - (diffusion(x, y) * dt)/(2 * dx**2) - (diffusion.partial_x(x, y) * dt)/(4 * dx)
-
+        return - r1(x, y) - r2(x, y)
     x_coords = C.xcoords
     u = np.zeros_like(x_coords)
     d = np.zeros_like(x_coords)
     l = np.zeros_like(x_coords)
     # 1-D vectors corresponding to upper, lower and middle diagonals
-    u[2: n_grid] = D3(x_coords[2: n_grid], y)
+    u[2: n_grid] = D3(x_coords[1: n_grid-1], y)
     d[1: n_grid-1] = D2(x_coords[1: n_grid-1], y)
-    l[0: n_grid-2] = D1(x_coords[0: n_grid-2], y)
+    l[0: n_grid-2] = D1(x_coords[1: n_grid-1], y)
     # Set boundary conditions
     if BC == 'neumann':
-        u[1] = 1
-        d[0] = -1
-        d[-1] = 1
-        l[-2] = -1
+        u[1] = -2*r2(x_coords[0], y)
+        d[0] = - D2(x_coords[0], y)
+        d[-1] = D2(x_coords[-1], y)
+        l[-2] = -2*r2(x_coords[-1], y)
     # Stack into a banded form
     ab = np.stack((u, d, l))
     return ab
@@ -74,29 +79,35 @@ def generate_left_matrix_y(C, diffusion, x, BC):
     dt = C.dt/2
     n_grid = C.n_grid
     # Define coefficient clusters
+    def r1(x, y):
+        return (diffusion(x, y) * dt)/(2 * dy**2)
+
+    def r2(x, y):
+        return (diffusion.partial_y(x, y) * dt)/(4 * dy)
+
     def D1(x, y):
-        return - (diffusion(x, y) * dt)/(2 * dy**2) + (diffusion.partial_y(x, y) * dt)/(4 * dy)
-    
+        return - r1(x, y) + r2(x, y)
+
     def D2(x, y):
-        return (diffusion(x, y)* dt)/(dy**2) + 1
+        return 2*r1(x, y) + 1
 
     def D3(x, y):
-        return - (diffusion(x, y) * dt)/(2 * dy**2) - (diffusion.partial_y(x, y) * dt)/(4 * dy)
+        return - r1(x, y) - r2(x, y)
 
     y_coords = C.ycoords
     u = np.zeros_like(y_coords)
     d = np.zeros_like(y_coords)
     l = np.zeros_like(y_coords)
     # 1-D vectors corresponding to upper, lower and middle diagonals
-    u[2: n_grid] = D3(x, y_coords[2: n_grid])
+    u[2: n_grid] = D3(x, y_coords[1: n_grid-1])
     d[1: n_grid-1] = D2(x, y_coords[1: n_grid-1])
-    l[0: n_grid-2] = D1(x, y_coords[0: n_grid-2])
+    l[0: n_grid-2] = D1(x, y_coords[1: n_grid-1])
     # Set boundary conditions
     if BC == 'neumann':
-        u[1] = 1
-        d[0] = -1
-        d[-1] = 1
-        l[-2] = -1
+        u[1] = -2*r2(x, y_coords[0])
+        d[0] = - D2(x, y_coords[0])
+        d[-1] = D2(x, y_coords[-1])
+        l[-2] = -2*r2(x, y_coords[-1])
     # Stack into a banded form
     ab = np.stack((u, d, l))
     return ab
@@ -120,14 +131,21 @@ def generate_right_matrix_x(C, diffusion, y, BC):
     n_grid = C.n_grid
     xcoords = C.xcoords
     # Define coefficient clusters
+
+    def r1(x, y):
+        return (diffusion(x, y) * dt)/(2 * dx**2)
+
+    def r2(x, y):
+        return (diffusion.partial_x(x, y) * dt)/(4 * dx)
+
     def D1(x, y):
-        return (diffusion(x, y) * dt)/(2 * dx**2) - (diffusion.partial_x(x, y) * dt)/(4 * dx)
+        return r1(x, y) - r2(x, y)
     
     def D2(x, y):
-        return -(diffusion(x, y) * dt)/(dx**2) + 1
+        return -2*r1(x, y) + 1
 
     def D3(x, y):
-        return (diffusion(x, y) * dt)/(2 * dx**2) + (diffusion.partial_x(x, y) * dt)/(4 * dx)
+        return r1(x, y) + r2(x, y)
 
     matrix = np.zeros((n_grid, n_grid))
     
@@ -138,7 +156,10 @@ def generate_right_matrix_x(C, diffusion, y, BC):
 
     # Set neumann boundary conditions; C1 - C0 = 0 by leaving 1st and last row empty
     if BC == 'Neumann':
-        return matrix
+        matrix[0, 0] = D2(xcoords[0], y)
+        matrix[0, 1] = 2*r1(xcoords[0], y)
+        matrix[-1, -2] = 2*r1(xcoords[-1], y)
+        matrix[-1, -1] = D2(xcoords[-1], y)
 
     else:
         matrix[0, 1:3] = np.array([-1, 1])
@@ -163,14 +184,21 @@ def generate_right_matrix_y(C, diffusion, x, BC):
     n_grid = C.n_grid
     ycoords = C.ycoords
     # Define coefficient clusters
+
+    def r1(x, y):
+        return (diffusion(x, y) * dt)/(2 * dy**2)
+
+    def r2(x, y):
+        return (diffusion.partial_y(x, y) * dt)/(4 * dy)
+
     def D1(x, y):
-        return (diffusion(x, y) * dt)/(2 * dy**2) - (diffusion.partial_y(x, y) * dt)/(4 * dy)
+        return r1(x, y) - r2(x, y)
     
     def D2(x, y):
-        return -(diffusion(x, y) * dt)/(dy**2) + 1
+        return -2*r1(x, y) + 1
 
     def D3(x, y):
-        return (diffusion(x, y) * dt)/(2 * dy**2) + (diffusion.partial_x(x, y) * dt)/(4 * dy)
+        return r1(x, y) + r2(x, y)
 
     matrix = np.zeros((n_grid, n_grid))
     
@@ -181,6 +209,10 @@ def generate_right_matrix_y(C, diffusion, x, BC):
 
     # Set open neumann boundary conditions; C1 - C0 = C2 - C1
     if BC == 'neumann':
+        matrix[0, 0] = D2(x, ycoords[0])
+        matrix[0, 1] = 2*r1(x, ycoords[0])
+        matrix[-1, -2] = 2*r1(x, ycoords[-1])
+        matrix[-1, -1] = D2(x, ycoords[-1])
         return matrix
     
     else:
@@ -207,18 +239,29 @@ def generate_explicit_comp_x(C, diffusion, j, BC):
     dt = C.dt/2
     diff_vec = diffusion(x_coords, y) # diffusion needs to be a vectorized function
     grad_diff_vec = diffusion.partial_y(x_coords, y)
-    C_jp1 = C.now[:, j+1] 
-    C_j = C.now[:, j]
-    C_jm1 = C.now[:, j-1]
 
-    term1 = (dt/dy**2) * (diff_vec * (C_jp1 - 2*C_j + C_jm1))
-    term2 = dt/(2*dy) * (grad_diff_vec * (C_jp1 - C_jm1))
-    b = term1 + term2
-    if BC == 'neumann':
-        b[0] = 0
-        b[-1] = 0
-        return b
+    # Handle explicit direction boundary conditions
+    if j == 0:
+        if BC == 'neumann':
+            C_jp1 = C.now[:, j+1]
+            C_j = C.now[:, j]
+            return (dt/dy**2) * (diff_vec * (2*C_jp1 - 2*C_j))
+
+    elif j == (n_grid - 1):
+        if BC == 'neumann':
+            C_j = C.now[:, j]
+            C_jm1 = C.now[:, j-1]
+            return (dt/dy**2) * (diff_vec * (2*C_jm1 - 2*C_j))
+
     else:
+        C_jp1 = C.now[:, j+1] 
+        C_j = C.now[:, j]
+        C_jm1 = C.now[:, j-1]
+
+        term1 = (dt/dy**2) * (diff_vec * (C_jp1 - 2*C_j + C_jm1))
+        term2 = dt/(2*dy) * (grad_diff_vec * (C_jp1 - C_jm1))
+        b = term1 + term2
+        
         return b
 
 def generate_explicit_comp_y(C, diffusion, i, BC): 
@@ -239,18 +282,28 @@ def generate_explicit_comp_y(C, diffusion, i, BC):
     dt = C.dt/2
     diff_vec = diffusion(x, y_coords) # diffusion needs to be a vectorized function
     grad_diff_vec = diffusion.partial_x(x, y_coords)
-    C_ip1 = C.now[i+1,:] 
-    C_i = C.now[i,:]
-    C_im1 = C.now[i-1,:]
 
-    term1 = (dt/dx**2) * (diff_vec * (C_ip1 - 2*C_i + C_im1))
-    term2 = dt/(2*dx) * (grad_diff_vec * (C_ip1 - C_im1))
-    b = term1 + term2
-    if BC == 'neumann':
-        b[0] = 0
-        b[-1] = 0
-        return b
+    # Handle explicit direction boundary conditions
+    if i == 0:
+        if BC == 'neumann':
+            C_ip1 = C.now[i+1,:] 
+            C_i = C.now[i,:]
+            return (dt/dx**2) * (diff_vec * (2*C_ip1 - 2*C_i))
+
+    elif i == (n_grid - 1):
+        if BC == 'neumann':
+            C_i = C.now[i,:]
+            C_im1 = C.now[i-1,:]
+            return (dt/dx**2) * (diff_vec * (2*C_im1 - 2*C_i))
+
     else:
+        C_ip1 = C.now[i+1,:] 
+        C_i = C.now[i,:]
+        C_im1 = C.now[i-1,:]
+
+        term1 = (dt/dx**2) * (diff_vec * (C_ip1 - 2*C_i + C_im1))
+        term2 = dt/(2*dx) * (grad_diff_vec * (C_ip1 - C_im1))
+        b = term1 + term2
         return b
 
 
@@ -278,9 +331,9 @@ def ADI(
     # Generate matrices beforehand? Its alot of matrices to store...
     # Maybe store in a dictionary? 
 
-    for timestep in range(1, n_time-1):
+    for timestep in range(1, n_time):
         # Perform x-direction implicit
-        for j in range(1, n_grid - 1): #half-timestep
+        for j in range(0, n_grid): #half-timestep
             y = ycoords[j]
             C_i = C.now[:, j] # Slice the array
             ab = generate_left_matrix_x(C, diffusion, y, BC=BC)
@@ -296,15 +349,11 @@ def ADI(
                 print(d)
                 raise ValueError
             C.next[:, j] = C_next
-        # Handle explicit direction boundaries
-        if BC == 'neumann':
-            C.next[:, -1] = C.next[:, -2]
-            C.next[:, 0] = C.next[:, 1]
         # Skip over saving the half-timestep results
         C.shift()
 
         # Perform y-direction implicit
-        for i in range(1, n_grid-1):
+        for i in range(0, n_grids):
             x = xcoords[i]
             C_j = C.now[i, :] # Slice the array
             ab = generate_left_matrix_y(C, diffusion, x, BC=BC)
@@ -319,10 +368,6 @@ def ADI(
                 print(B)
                 print(d)
             C.next[i, :] = C_next
-        # Handle explicit direction boundaries
-        if BC == 'neumann':
-            C.next[-1, :] = C.next[-2, :]
-            C.next[0, :] = C.next[1, :]
 
         C.store_timestep(timestep)
         C.shift()
