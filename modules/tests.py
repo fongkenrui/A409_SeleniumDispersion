2104f2b31e1b71a1b3f344031d2e94f7e6613713
Ken <kenrui.fong@gmail.com>
2024-04-09 20:12:05 -0700

    Fixed flux calculations + add sources to tests

--- modules/tests.py
+++ modules/tests.py
@@ -32,23 +32,22 @@ def calculate_boundary_flux(the_ds):
         C = the_ds['concentration'].isel(t=n).values
         # Left Boundary
         cl0 = C[0,:]
-        cl1 = C[0,:]
-        Jl = - D[0, :] @ (cl0 - cl1)*(dy/dx)
+        cl1 = C[1,:]
+        Jl = D[0, :] @ (cl1 - cl0)*(dy/dx)
         # Right Boundary
         cr0 = C[-2,:]
         cr1 = C[-1,:]
-        Jr = - D[-1, :] @ (cr0 - cr1)*(dy/dx)
+        Jr = D[-1, :] @ (cr0 - cr1)*(dy/dx)
         # Top Boundary
         ct0 = C[:,0]
         ct1 = C[:, 1]
-        Jt = - D[:, 0] @ (ct0 - ct1)*(dx/dy)
+        Jt = D[:, 0] @ (ct1 - ct0)*(dx/dy)
         # Bottom Boundary
         cb0 = C[:,-2]
         cb1 = C[:,-1]
-        Jb = - D[:, 0] @ (cb1 - cb0)*(dx/dy)
+        Jb = D[:, 0] @ (cb0 - cb1)*(dx/dy)
 
         J_current = Jl + Jr + Jt + Jb
-        print(J_current)
         if n == 0:
             cumflux[n] = J_current*dt
         else:
@@ -67,9 +66,16 @@ def integrate_concentration(the_ds):
     dy = the_ds.attrs['dy']
     return the_ds['concentration'].sum(dim=('x', 'y')).values*(dy*dx)
 
+def integrate_sources(the_ds):
+    dx = the_ds.attrs['dx']
+    dy = the_ds.attrs['dy']
+    time = the_ds.coords['t'].values
+    return the_ds['sources'].sum(dim=('x', 'y')).values*(dy*dx) * time
+
 def plot_mass_conservation(the_ds):
-    """Plots total pollutant concentration, time-integrated flux, and the sum of both, normalized
-    by initial total concentration.
+    """Plots total pollutant concentration, time-integrated boundary flux, source influx, and conserved mass
+    normalized by the sum of conserved mass at t=0 + inflow. Conserved mass = total concentration + time-integrated
+    boundary flux.
 
     Args:
         the_ds (_type_): _description_
@@ -77,12 +83,17 @@ def plot_mass_conservation(the_ds):
     time = the_ds.coords['t']
     cumflux = calculate_boundary_flux(the_ds)
     totalconc = integrate_concentration(the_ds)
+    inflow = integrate_sources(the_ds)
+    conserved_mass = totalconc + cumflux 
     fig, ax = plt.subplots()
-    ax.plot(time, cumflux/totalconc[0], label='boundary flux')
-    ax.plot(time, totalconc/totalconc[0], label='mass in boundary')
-    ax.plot(time, (totalconc + cumflux)/totalconc[0], label='total mass')
+    ax.plot(time, cumflux/(conserved_mass[0] + inflow), label='cumulative boundary flux')
+    ax.plot(time, totalconc/(conserved_mass[0] + inflow), label='mass in boundary')
+    ax.plot(time, inflow/(conserved_mass[0] + inflow), label='cumulative inflow')
+    ax.plot(time, conserved_mass/(conserved_mass[0] + inflow), label='total mass')
+    ax.set_xlabel("time")
+    ax.set_ylabel("fractional mass")
     ax.legend()
-    ax.set_title("Mass Conservation Test")
+    ax.set_title("Mass fraction scaled by initial conserved mass + inflow")
     return fig, ax
 
 def test_gaussian(simfunc):
@@ -133,5 +144,6 @@ def test_gaussian(simfunc):
         },
     )
     diff = (result_ds - ads)/ads
+    diff.rename('relative error')
     return diff
 
