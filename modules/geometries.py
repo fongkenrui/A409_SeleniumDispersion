9bfe31630af061a2807809bfb6cc6e7f342894e2
Yuki Torigata <124737973+ytorigata@users.noreply.github.com>
2024-04-12 13:15:03 -0700

    Soil geometry plotting (#30)
    * soil matrix
    
    * square matrices
    
    * geometry for 3 types
    
    * 3
    
    * Added a return statement for each function
    
    * Fixed indent
    
    * Fixed typo
    
    * Reduced the number of the elements of the array
    
    * Changed the function names
    
    * Added a visualization function for soil matrices
    
    * Changed to use the given arguments to create triangles
    
    * Separate the part calculating diffusion
    
    ---------
    
    Co-authored-by: Sayana <sayana.kirk@gmail.com>

--- modules/geometries.py
+++ modules/geometries.py
@@ -1,11 +1,53 @@
 import numpy as np 
+from modules.classes import Interpolate
+
+def create_soil_geometry(conc, pattern_func, args, D_background):
+    """Create soil geometry baased on a seed soil matrix"""
+    n_grid_pt = 10
+
+    seed_matrix = pattern_func(*args)
+    
+    if seed_matrix.shape[0] != seed_matrix.shape[1]:
+        raise('A seed soil matrix must be a square matrix.')
+        
+    elif (seed_matrix.shape[0] != n_grid_pt) | (seed_matrix.shape[1] != n_grid_pt):
+        # if the seed matrix is smaller than a n_grid_pt-square matrix, adjust the size
+        
+        background_matrix = np.full((n_grid_pt, n_grid_pt), D_background)
+        idx = n_grid_pt // 2 - seed_matrix.shape[0] // 2
+        background_matrix[idx:-idx, idx:-idx] = seed_matrix
+        seed_matrix = background_matrix
+
+    xint = np.linspace(-0.5 * conc.n_grid * conc.dx, 0.5 * conc.n_grid * conc.dx, n_grid_pt)
+    yint = np.linspace(-0.5 * conc.n_grid * conc.dy, 0.5 * conc.n_grid * conc.dy, n_grid_pt)
+    diffusion = Interpolate(seed_matrix, xint, yint, s=0)
+    
+    return diffusion
+
+
+def plot_soil_geometry(conc, pattern_func, args, D_background):
+    """Display soil geometry"""
+
+    diffusion = create_soil_geometry(conc, pattern_func, args, D_background)
+    fig, ax = diffusion.plot_color_map(func='func')
+    
+    return fig, ax
+
 
 def triangle_2(a,b):
     """Return a soil matrix of a triangle pattern"""
     
-    a = np.full((10, 10), 0.02)  
-    b = np.tril(a)
-    matrix = a + b
+    a_matrix = np.full((10, 10), a)  
+    lower_triangle = np.tril(a_matrix)
+    
+    b_matrix = np.full((10, 10), b) 
+    upper_triangle = np.triu(b_matrix)
+    
+    matrix = lower_triangle + upper_triangle
+    
+    for i in range(len(matrix)):
+        matrix[i, i] = min(upper_triangle[i, i], lower_triangle[i, i])
+
     return matrix
 
 
